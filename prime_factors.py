d8b62c58d8b4bf359ffc88e2f0cb5bb52e8ed9bd
Javad Hamidi <Vadeghan@users.noreply.github.com>
2017-10-13 12:00:49 +1030

    Create prime_factors.py

---
+++ prime_factors.py
@@ -0,0 +1,20 @@
+"""
+For this week's challenge
+Write a function that finds the sum of all prime factors of a given number (n)
+
+Advanced Extension: Try to write your function without using trial division!
+	- https://en.wikipedia.org/wiki/Fermat's_factorization_method
+  - https://en.wikipedia.org/wiki/Quadratic_sieve
+  - http://en.wikipedia.org/wiki/Pollard's_rho_algorithm
+  - https://en.wikipedia.org/wiki/Dixon's_factorization_method
+
+"""
+def primeFactor(n):
+  pFact = []
+  for i in range(1, n):
+    remainder = n%i
+  	if remainder == 0:
+      pFact.append(i)
+  return pFact
+
+print(primeFactor(int(input("Give us a number!!!:)"))))
